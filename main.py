ae5e8f6a3af87202ee3a1fca9aaca7df290cdfc7
AngelTravieso <angeltraviesoc@gmail.com>
2022-04-27 23:52:43 -0400

    basicos python

---
+++ main.py
@@ -0,0 +1,105 @@
+# miVariable = 2
+#
+# print(miVariable)
+# print(miVariable)
+# print(miVariable)
+#
+# miVariable = 10
+# print(miVariable)
+
+# x = 10
+# y = 2
+# z = x + y
+#
+# print(x)
+# print(y)
+# print(z)
+#
+# print(id(x))
+# print(id(y))
+# print(id(z))
+
+
+# Python - Data Types
+
+# int
+# x = 10
+# print(type(x))
+#
+# # float
+# x = 14.5
+# print(type(x))
+#
+# # str
+# x = 'Hola Mundo'
+# print(type(x))
+#
+# # bool
+# x = True
+# print(type(x))
+
+
+# Manejo de Cadenas
+
+# Cadena (str)
+# miGrupoFav = 'Metallica'
+# comentario = 'The Best Rock Band'
+#
+# #print('Mi grupo favorito es: ' + miGrupoFav + ' ' + comentario)
+#
+# print('Mi grupo favorito es:', miGrupoFav, comentario)
+#
+# numero1 = '1'
+# numero2 = '2'
+# print('Concatenación:', numero1 + numero2)
+#
+# print('Suma:', int(numero1) + int(numero2))
+#
+# # numero1 = 1
+# # numero2 = 2
+# # print(numero1 + numero2)
+
+
+# Tipos bool (booleano)
+
+# miVariable = False
+# print(miVariable)
+#
+# miVariable = 3 > 2
+# print(miVariable)
+#
+# if miVariable:
+#     print('El resultado fue verdadero')
+# else:
+#     print('El resultado fue false')
+
+
+# Procesar entrada de Usuario (input)
+# Función input para procesar la entrada del usuario
+
+# resultado = input('Escribe un mensaje: ')
+# print('Valor proporcionado:', resultado)
+# print('Fin del programa')
+
+# numero1 = int(input('Escribe el primer número: '))
+# numero2 = int(input('Escribe el segundo número: '))
+# resultado = numero1 + numero2
+# print('El resultado de la suma es:', resultado)
+
+
+# Ejercicio: Califica tu día del 1 al 10
+# calificacion = int(input('Cómo estuvo tu día (1 al 10):'))
+# print('Mi día estuvo de:', calificacion)
+
+
+
+
+
+
+
+
+
+
+
+
+
